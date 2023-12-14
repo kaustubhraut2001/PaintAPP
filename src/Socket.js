70afa95ec6fc76602dd4997c78bd7751a7adb789
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-12-14 15:01:18 +0530

    some env fixes

--- src/Socket.js
+++ src/Socket.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const envirnmentt = NEXT_PUBLIC_ENVIRNMENT ? 'production' : 'development';
+const envirnmentt = process.env.NEXT_PUBLIC_ENVIRNMENT ? 'production' : 'development';
 const url = envirnmentt === 'production' ? 'https://drawingtool.onrender.com' : 'http://localhost:5000';
 
 export const socket = io(url);
